d7d2996f1d11a89d4419ffb2099295b6091cc3f0
SalfaBella <salfabella64@gmail.com>
2023-01-13 12:46:13 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas4reactiveprogramming/activity_home.kt
@@ -0,0 +1,11 @@
+package com.example.tugas4reactiveprogramming
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class activity_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
